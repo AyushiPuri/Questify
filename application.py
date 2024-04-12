c928b56de87a091357363e0c48285c1d695926b2
AyushiPuri <puriayushi6@gmail.com>
2024-04-12 16:58:10 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,redirect,request
+from database import user,add_to_db,get_db
+app = Flask(__name__)
+app.secret_key="themostsecretkeyever"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/example1/form',methods=['GET','POST'])
+def example_form():
+    status=None
+    if request.method=='POST':
+        name=request.form.get('uname')
+        email=request.form.get('email')
+        password=request.form.get('pwd')
+        if len(email)>0 and len(name)>0 and len (password)>0:
+            u=user(name=name,email=email,password=password)
+            add_to_db(u)
+            status="user added successfully"
+        else:
+            status="fill all fields"  
+    return render_template('form.html',status=status)
+
+@app.route('/example1/list')
+def example_1_list():
+    db=get_db()
+    users = db.query(user).all()
+    return render_template('list.html',userlist=users)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
